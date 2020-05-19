9faa6e0c9b732eef4dbc09c08a7f92bb326d168a
Michael P Nance <michael.p.nance@gmail.com>
2020-05-19 17:20:58 -0400

    DELETE /api/notes/:id route implemented

--- Develop/server.js
+++ Develop/server.js
@@ -18,7 +18,7 @@ const DBPATH = "./db/db.json";
 
 // Routes
 server.get("/notes", (req, res) => res.sendFile(path.join(__dirname, "/public/notes.html")));
-server.get("*", (req, res) => res.sendFile(path.join(__dirname, "/public/index.html")));
+// server.get("*", (req, res) => res.sendFile(path.join(__dirname, "/public/index.html")));
 
 // GET `/api/notes` - Should read the `db.json` file and return all saved notes as JSON.
 server.get("/api/notes", (req, res) => {
@@ -45,10 +45,15 @@ server.post("/api/notes", (req, res) => {
 });
 
 // DELETE `/api/notes/:id` - Should receive a query parameter containing the id of a note to delete. This means you'll need to find a way to give each note a unique `id` when it's saved. In order to delete a note, you'll need to read all notes from the `db.json` file, remove the note with the given `id` property, and then rewrite the notes to the `db.json` file.
-
-
+server.delete("/api/notes/:id", (req, res) => {
+    fs.readFile(DBPATH, "utf8", (err, data) => {
+        const output = JSON.stringify(JSON.parse(data).filter(note => note.id !== req.params.id));
+        fs.writeFile(DBPATH, output, (err) => { if (err) console.log(err) });
+    });
+    return res.send(req.body.id);
+});
 
 // Start Server
 server.listen(port, function() {
-    console.log("App listening on PORT: " + port);
+    console.log("Server listening on PORT: " + port);
 });
