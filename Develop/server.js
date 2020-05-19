2d5c72c62174783822a8e17fc00a1c6958131087
Michael P Nance <michael.p.nance@gmail.com>
2020-05-19 16:56:35 -0400

    updated /api/notes route to add unique id's to notes

--- Develop/server.js
+++ Develop/server.js
@@ -4,7 +4,7 @@
 const express = require("express");
 const path = require("path");
 const fs = require("fs");
-const uuid = require("uuid");
+const { v4: uuid } = require("uuid");
 
 // Server Configuration
 const server = express();
@@ -18,7 +18,7 @@ const DBPATH = "./db/db.json";
 
 // Routes
 server.get("/notes", (req, res) => res.sendFile(path.join(__dirname, "/public/notes.html")));
-// server.get("*", (req, res) => res.sendFile(path.join(__dirname, "/public/index.html")));
+server.get("*", (req, res) => res.sendFile(path.join(__dirname, "/public/index.html")));
 
 // GET `/api/notes` - Should read the `db.json` file and return all saved notes as JSON.
 server.get("/api/notes", (req, res) => {
@@ -32,13 +32,19 @@ server.get("/api/notes", (req, res) => {
 server.post("/api/notes", (req, res) => {
     fs.readFile(DBPATH, "utf8", (err, data) => {
         if (err) console.log(err);
-        const output = JSON.stringify(JSON.parse(data).concat(req.body));
+        // Create note object corresponding to req data with unique id
+        const note = {
+            title: req.body.title,
+            text: req.body.text,
+            id: uuid(),
+        };
+        const output = JSON.stringify(JSON.parse(data).concat(note));
         fs.writeFile(DBPATH, output, (err) => { if (err) console.log(err) });
     });
     return res.json(req.body);
 });
 
-
+// DELETE `/api/notes/:id` - Should receive a query parameter containing the id of a note to delete. This means you'll need to find a way to give each note a unique `id` when it's saved. In order to delete a note, you'll need to read all notes from the `db.json` file, remove the note with the given `id` property, and then rewrite the notes to the `db.json` file.
 
 
 
